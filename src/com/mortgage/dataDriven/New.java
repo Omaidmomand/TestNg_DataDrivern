1e52736040fce19e56e653c4a96208d9fec672ec
Omaidmomand <prince.deen@hotmail.com>
2020-05-14 09:41:16 -0400

    added new class

---
+++ src/com/mortgage/dataDriven/New.java
@@ -0,0 +1,15 @@
+package com.mortgage.dataDriven;
+
+public class New {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a = 34;
+		int b = 56;
+		int c = a+b;
+		System.out.println(c);
+	}
+
+}
